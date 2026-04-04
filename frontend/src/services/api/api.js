dfe1e821dd855769551d671142fdec058d96bc7b
huybee1403 <giahuy14022003@gmail.gmailcom>
2026-04-05 00:40:24 +0700

    Authentication API
    DA-13 DA-37 DA-38 DA-39 DA-42: Register API, Login API, Logout, Role middleware

--- frontend/src/services/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "../../utils/axios.customize";
-
-// === Auth API ===
-export const registerAPI = (data) => axios.post(`/auth/register`, data).then((res) => res.data);
-export const loginAPI = (data) => axios.post(`/auth/login`, data).then((res) => res.data);
-export const forgotPasswordAPI = (data) => axios.post(`/auth/forgot-password`, data).then((res) => res.data);
-export const resetPasswordAPI = (token, new_password) => axios.post(`/auth/reset-password`, { token, new_password }).then((res) => res.data);
-export const logoutAPI = () => axios.post(`/auth/logout`).then((res) => res.data);
-export const refreshTokenAPI = () => axios.post("/auth/refresh-token").then((res) => res.data);
